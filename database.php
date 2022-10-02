92ce0a8a8abfa2b1f4f75e2acfba4adf6f03c72a
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-10-02 20:44:52 +0300

    database changes

--- database.php
+++ database.php
@@ -6,11 +6,25 @@
     $db = substr($url["path"], 1);
 
     class Database {
+
+        private $url;
+        public $server;
+        public $username;
+        public $password;
+        public $db;
+
+
         public function dbConnect()
         {
+            $this->$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $this->$server = $url["host"];
+            $this->$username = $url["user"];
+            $this->$password = $url["pass"];
+            $this->$db = substr($url["path"], 1);
+
             try 
             {
-                $conn = new PDO('mysql:host=' . $server . ';dbname=' . $db, $username, $password);
+                $conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->db, $this->username, $this->password);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $conn;
             } 
